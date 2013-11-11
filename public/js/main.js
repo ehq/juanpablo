c10fd619f0c6a1eb8f407a96d254fa72f3ef5557
Bocha <jpablotroncoso@gmail.com>
2013-11-10 22:41:33 -0300

    Add new images.

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,61 @@
 (function() {
     $(document).ready(function() {
+        $(window).scroll(function() {
 
+            var topOfWindow = $(window).scrollTop();
+
+            $('.bar').each(function(){
+                var imagePos = $(this).offset().top;
+
+                if (imagePos < topOfWindow+700) {
+                    $(this).addClass("pullUp");
+                }
+            });
+
+            $('#creation figure').each(function(){
+                var imagePos = $(this).offset().top;
+
+                if (imagePos < topOfWindow+400) {
+                    $(this).addClass("show");
+                    $(this).addClass("slideUp");
+                }
+            });
+
+            $('span.arrow_right').each(function(){
+                var imagePos = $(this).offset().top;
+
+                if (imagePos < topOfWindow+400) {
+                    $(this).addClass("stretchRight");
+                }
+            });
+
+            $('#hobbies figure').each(function(){
+                var imagePos = $(this).offset().top;
+
+                if (imagePos < topOfWindow+400) {
+                    $(this).addClass("slideLeft");
+                }
+            });
+
+            $('.eyes').each(function(){
+                var imagePos = $(this).offset().top;
+
+                if (imagePos < topOfWindow+400) {
+                    $(this).addClass("floating");
+                }
+            });
+        });
+
+        $('header').addClass('show');
+
+        // Docs sidebar list functionality.
+        $('.scrollDown, .scrollUp').click(function(){
+          // Scroll to the anchored section.
+          $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+          }, 500);
+          return false;
+        });
 
     });
 })(jQuery);
