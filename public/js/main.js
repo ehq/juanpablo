06ab91d8759f514a00238b6b367471249a91c0e5
Bocha <jpablotroncoso@gmail.com>
2013-12-16 18:20:29 -0300

    Finish creation figures. Add contact icons.

--- public/js/main.js
+++ public/js/main.js
@@ -52,19 +52,44 @@
                     $(this).addClass("show");
                 }
             });
+
+            $('button').each(function(){
+                var imagePos = $(this).offset().top;
+
+                if (imagePos < topOfWindow+400) {
+                    $(this).addClass("shake");
+                }
+            });
+
+            $('#contact a').each(function(){
+                var imagePos = $(this).offset().top;
+
+                if (imagePos < topOfWindow+800) {
+                    $(this).addClass("flipInX");
+                }
+            });
+
+            $('#contact a').hover(function(){
+                $(this).removeClass("flipInX");
+                $(this).toggleClass("bounce");
+            });
+
+
         });
 
         $('#presentation header').addClass('show');
 
         $('.scrollTo').click(function(){
-          // Scroll to the anchored section.
-          $('html, body').animate({
-            scrollTop: $( $.attr(this, 'href') ).offset().top
-          }, 500);
-          $('.scrollTo, p').removeClass('selected');
-          $(this).addClass('selected');
-          $(this).prev().addClass('selected');
-          return false;
+            // Scroll to the anchored section.
+            $('html, body').animate({
+                scrollTop: $( $.attr(this, 'href') ).offset().top
+            }, 500);
+
+            $('.scrollTo, p').removeClass('selected');
+            $(this).addClass('selected');
+            $(this).prev().addClass('selected');
+            
+            return false;
         });
 
     });
